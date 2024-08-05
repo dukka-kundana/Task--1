5e909160d09cc1a26d66b7ed2b1cbde786ead90d
dukka-kundana <137527373+dukka-kundana@users.noreply.github.com>
2024-08-05 21:40:25 +0530

    Add files via upload

---
+++ Task- 1/scripts.js
@@ -0,0 +1,24 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const sections = document.querySelectorAll('.parallax-section');
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in');
+            observer.unobserve(entry.target);
+        }
+    });
+}, { threshold: 0.1 });
+
+sections.forEach(section => {
+    observer.observe(section);
+});
